24e0bd400e58cf01cd9a60c5356cf78a68787b3d
cxlos <bautistac_29@outlook.com>
2024-05-21 00:44:37 -0500

    minor style changes to charts

--- flight_delay.py
+++ flight_delay.py
@@ -60,92 +60,125 @@ server= app.server
 # Add a html.Div and core input text component
 # Finally, add graph component.
 # Build dash app layout
-app.layout = html.Div(children=[ 
-    
-                    # Title
-                    html.H1('Flight Delay Time Statistics', 
-                      style={'textAlign': 'center', 'color': 'cadetblue',
-                      'fontSize': 45, 'font-family':'Calibri'}),
 
-                    # Input box
-                    html.Div(["Input Year: ", dcc.Input(id='input-year', value='2010', 
-                      type='number', style={'height':'35px', 'font-size': 30}),], 
-                      style={'font-size': 30, 'font-family':'Calibri'}),
+# Define the layout of the app
+app.layout = html.Div(children=[
+    # Title
+    html.H1(
+        'Flight Delay Time Statistics',
+        style={'textAlign': 'center', 'color': 'cadetblue', 'fontSize': 45, 'font-family': 'Calibri'}
+    ),
 
-                    html.Br(),
-                    html.Br(), 
+    # Input box
+    html.Div(
+        [
+            html.Span(
+                "Input Year: ",
+                style={'margin-left': '20px'}
+            ),
+            dcc.Input(
+                id='input-year',
+                value='2010',
+                type='number',
+                style={'height': '35px', 'font-size': 30, 'font-family': 'Calibri'}
+            )
+        ],
+        style={'font-size': 30, 'font-family': 'Calibri'}
+    ),
 
-                    # Segment 1
-                    html.Div([
-                              html.Div(dcc.Graph(id='carrier-plot')),
-                              html.Div(dcc.Graph(id='weather-plot'))],
-                                style={'display': 'flex'}),
+    html.Br(),
+    html.Br(),
 
-                    # Segment 2
-                      html.Div([
-                              html.Div(dcc.Graph(id='nas-plot')),
-                              html.Div(dcc.Graph(id='security-plot'))],
-                                style={'display': 'flex'}),
+    # Segment 1
+    html.Div(
+        [
+            html.Div(dcc.Graph(id='carrier-plot')),
+            html.Div(dcc.Graph(id='weather-plot'))
+        ],
+        style={'display': 'flex'}
+    ),
 
-                    # Segment 3
-                    html.Div(
-                              dcc.Graph(id='late-plot'),
-                                style={'width':'65%'})
-                    ])
+    # Segment 2
+    html.Div(
+        [
+            html.Div(dcc.Graph(id='nas-plot')),
+            html.Div(dcc.Graph(id='security-plot'))
+        ],
+        style={'display': 'flex'}
+    ),
 
-# Add computation to callback function and return graph
+    # Segment 3
+    html.Div(
+        dcc.Graph(id='late-plot'),
+        style={'width': '65%', 'margin': '0 auto'}
+    )
+])
+
+# Define the function to compute the necessary data
 def compute_info(airline_data, entered_year):
-    
     # Select data
-    df =  airline_data[airline_data['Year']==int(entered_year)]
-    
+    df = airline_data[airline_data['Year'] == int(entered_year)]
+
     # Compute delay averages
-    avg_car = df.groupby(['Month','Reporting_Airline'])['CarrierDelay'].mean().reset_index()
-    avg_weather = df.groupby(['Month','Reporting_Airline'])['WeatherDelay'].mean().reset_index()
-    avg_NAS = df.groupby(['Month','Reporting_Airline'])['NASDelay'].mean().reset_index()
-    avg_sec = df.groupby(['Month','Reporting_Airline'])['SecurityDelay'].mean().reset_index()
-    avg_late = df.groupby(['Month','Reporting_Airline'])['LateAircraftDelay'].mean().reset_index()
+    avg_car = df.groupby(['Month', 'Reporting_Airline'])['CarrierDelay'].mean().reset_index()
+    avg_weather = df.groupby(['Month', 'Reporting_Airline'])['WeatherDelay'].mean().reset_index()
+    avg_NAS = df.groupby(['Month', 'Reporting_Airline'])['NASDelay'].mean().reset_index()
+    avg_sec = df.groupby(['Month', 'Reporting_Airline'])['SecurityDelay'].mean().reset_index()
+    avg_late = df.groupby(['Month', 'Reporting_Airline'])['LateAircraftDelay'].mean().reset_index()
     return avg_car, avg_weather, avg_NAS, avg_sec, avg_late
 
-# CALLBACK DECORATOR
-
+# Define the callback function to update the graphs
 @app.callback([Output(component_id='carrier-plot', component_property='figure'),
                Output(component_id='weather-plot', component_property='figure'),
                Output(component_id='nas-plot', component_property='figure'),
                Output(component_id='security-plot', component_property='figure'),
                Output(component_id='late-plot', component_property='figure')],
-               Input(component_id='input-year', component_property='value'))
-               
-
-# Computation to callback function and return graph
+              [Input(component_id='input-year', component_property='value')])
 def get_graph(entered_year):
-    
     # Compute required information for creating graph from the data
     avg_car, avg_weather, avg_NAS, avg_sec, avg_late = compute_info(airline_data, entered_year)
-            
+
     # Line plot for carrier delay
     carrier_fig = px.line(avg_car, x='Month', y='CarrierDelay', color='Reporting_Airline')
     carrier_fig.update_layout(
-    title={
-        'text': 'Average carrrier delay time (minutes) by airline',
-        'x': 0.45},  # Set title position to be centered
-    xaxis_title='Month',
-    yaxis_title='Delay'
-)
-    # Line plot for weather delay
-    weather_fig = px.line(avg_weather, x='Month', y='WeatherDelay', color='Reporting_Airline', title='Average weather delay time (minutes) by airline')
+        title={'text': 'Average carrier delay time (minutes) by airline', 'x': 0.45},
+        xaxis_title='Month',
+        yaxis_title='Delay'
+    )
 
-    # Line plot for nas delay
-    nas_fig = px.line(avg_NAS, x='Month', y='NASDelay', color='Reporting_Airline', title='Average NAS delay time (minutes) by airline')
+    # Create the weather delay figure
+    weather_fig = px.line(avg_weather, x='Month', y='WeatherDelay', color='Reporting_Airline')
+    weather_fig.update_layout(
+        title={'text': 'Average weather delay time (minutes) by airline', 'x': 0.45},
+        xaxis_title='Month',
+        yaxis_title='Delay'
+    )
 
-    # Line plot for security delay
-    sec_fig = px.line(avg_sec, x='Month', y='SecurityDelay', color='Reporting_Airline', title='Average security delay time (minutes) by airline')
+    # Create the NAS delay figure
+    nas_fig = px.line(avg_NAS, x='Month', y='NASDelay', color='Reporting_Airline')
+    nas_fig.update_layout(
+        title={'text': 'Average NAS delay time (minutes) by airline', 'x': 0.45},
+        xaxis_title='Month',
+        yaxis_title='Delay'
+    )
 
-    # Line plot for late aircraft delay
-    late_fig = px.line(avg_late, x='Month', y='LateAircraftDelay', color='Reporting_Airline', title='Average late aircraft delay time (minutes) by airline')
-            
-    return[carrier_fig, weather_fig, nas_fig, sec_fig, late_fig]
-               
+    # Create the security delay figure
+    sec_fig = px.line(avg_sec, x='Month', y='SecurityDelay', color='Reporting_Airline')
+    sec_fig.update_layout(
+        title={'text': 'Average security delay time (minutes) by airline', 'x': 0.45},
+        xaxis_title='Month',
+        yaxis_title='Delay'
+    )
+
+    # Create the late aircraft delay figure
+    late_fig = px.line(avg_late, x='Month', y='LateAircraftDelay', color='Reporting_Airline')
+    late_fig.update_layout(
+        title={'text': 'Average late aircraft delay time (minutes) by airline', 'x': 0.45},
+        xaxis_title='Month',
+        yaxis_title='Delay'
+    )
+
+    return [carrier_fig, weather_fig, nas_fig, sec_fig, late_fig]
 
 # Run the app
 if __name__ == '__main__':
